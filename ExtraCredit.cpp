12bc57c9e3a77a5f24191e0493ce423cd3937bf5
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-23 15:11:20 -0400

    removed to_string, using string instead

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -10,32 +10,30 @@ istringstream iss(input);
 
 char openParan, closeParan, separator;
 
-int areaCode, firstPart, secondPart;
+string areaCode, firstPart, secondPart;
 
 bool isValid{};
 
 
 if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) {
-    string areaCodeString = to_string(areaCode);
-    string firstPartString = to_string(firstPart);
-    string secondPartString = to_string(secondPart);
+
     //tostring from int to string
     isValid = true;
     
-    for (int i = 0; i < areaCodeString.length(); i++) {
-            if (!isdigit(areaCodeString[i])) {
+    for (int i = 0; i < areaCode.length(); i++) {
+            if (!isdigit(areaCode[i])) {
                 isValid = false;
                 break;
             }
         }
-    for (int i = 0; i < firstPartString.length(); i++) {
-            if (!isdigit(firstPartString[i])) {
+    for (int i = 0; i < firstPart.length(); i++) {
+            if (!isdigit(firstPart[i])) {
                 isValid = false;
                 break;
             }
         }
-    for (int i = 0; i < secondPartString.length(); i++) {
-            if (!isdigit(secondPartString[i])) {
+    for (int i = 0; i < secondPart.length(); i++) {
+            if (!isdigit(secondPart[i])) {
                 isValid = false;
                 break;
             }
@@ -46,7 +44,7 @@ if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> seco
         if (openParan == '(' && closeParan == ')' && separator == '-' &&
     areaCodeString.length() == 3 && 
     firstPartString.length()==3 && 
-    secondPartString.length()==4) {
+    secondPartString.length()==4 ) {
             return isValid;
     }
     //should only be able to run if all the strings are completely valid, all integers no mixed inputs "00A" or "OO0" etc.
