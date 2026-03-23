190b24a0a115f95f972ec034a7eddc971b64415c
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-23 15:33:44 -0400

    maybe a fix. substring of firstPart should be split into the second part and first parts.

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -15,7 +15,14 @@ string areaCode, firstPart, secondPart;
 bool isValid{};
 
 
-if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) {
+if (iss >> openParan >> areaCode >> closeParan >> firstPart) {
+    secondPart = firstPart.substr(firstPart.find('-') + 1);
+    firstPart  = firstPart.substr(0, firstPart.find('-'));
+    //(xxx) xxx-xxxx the later xxx-xxxx becomes merged all into firstPart for some reason? 
+    //This should fix it hopefully
+ 
+
+
 
     //tostring from int to string
     isValid = true;
