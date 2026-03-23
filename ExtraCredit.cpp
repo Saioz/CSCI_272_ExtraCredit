c459dfb84eb2b9548962ef733eb92ee992308e1e
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-23 13:53:59 -0400

    fixed syntax?

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -7,20 +7,24 @@ bool isValidPhoneNumber(const string& input) {
 istringstream iss(input);
 
 char openParan, closeParan, separator;
+    string areaCode, firstPart, secondPart;
 
-string areaCode, firstPart, secondPart;
-    if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) { 
-        if (areaCode.length() == 3 && firstPart.length()==3 && secondPart.length()==4) {
+    if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) {
+        if (areaCode.length() == 3 && firstPart.length() == 3 && secondPart.length() == 4) {
+
+
+
             return true;
+        }
+        return false;
     }
-    return false; 
-}
+    return false;
 }
 
 
 int main() {
     string input;
-        cout << "Enter a U.S phone number xxx - xxx - xxxx: ";
+        cout << "Enter a U.S phone number (xxx) xxx-xxxx: ";
         getline(cin, input);
     
     if (isValidPhoneNumber(input)) {
