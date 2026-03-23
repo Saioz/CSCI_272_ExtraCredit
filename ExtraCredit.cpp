b7bde0628cef431083b5070f2cfc04e7412a9a0e
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-23 14:26:21 -0400

    try try again

--- ExtraCredit.cpp
+++ ExtraCredit.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -7,35 +8,45 @@ bool isValidPhoneNumber(const string& input) {
 istringstream iss(input);
 
 char openParan, closeParan, separator;
-    string areaCode, firstPart, secondPart;
 
-    if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) {
-        if (areaCode.length() == 3 && firstPart.length() == 3 && secondPart.length() == 4) {
+int areaCode, firstPart, secondPart;
 
+if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) {
 
-
+    string areaCodeString = to_string(areaCode);
+    string firstPartString = to_string(firstPart);
+    string secondPartString = to_string(secondPart);
+    
+    
+    
+    if (openParan == '(' && closeParan == ')' && separator == '-' &&
+    areaCodeString.length() == 3 && 
+    firstPartString.length()==3 && 
+    secondPartString.length()==4 &&
+    areaCode <=999 &&
+    firstPart <= 999 &&
+    secondPart <=9999)
+    {
             return true;
-        }
+    }
         return false;
     }
     return false;
 }
 
-
 int main() {
-    string input;
-        cout << "Enter a U.S phone number (xxx) xxx-xxxx: ";
-        getline(cin, input);
-    
+string input;
+
+    cout << "Enter a U.S phone number xxx - xxx - xxxx: ";
+    getline(cin, input);
+
     if (isValidPhoneNumber(input)) {
         cout << "Valid." << endl;
-    
     }
     else {
         cout << "Invalid." << endl;
-    
+
     }
-    return 0;
+        return 0;
+
 }
-
-
