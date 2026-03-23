faaed7bf8caf4a0697841bb9d1bb70ff7b2f0c3a
Sai0 <77402820+Saioz@users.noreply.github.com>
2026-03-23 13:39:39 -0400

    initial practice

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool isValidPhoneNumber(const string& input) {
+istringstream iss(input);
+
+char openParan, closeParan, separator;
+
+string areaCode, firstPart, secondPart;
+
+    
+    
+    if (iss >> openParan >> areaCode >> closeParan >> firstPart >> separator >> secondPart) { 
+        if (areaCode.length() == 3 && firstPart.length()==3 && secondPart.length()==4) {
+            return true;
+    }
+    return false; 
+}
+}
+
+
+int main() {
+    string input;
+        cout << "Enter a U.S phone number xxx - xxx - xxxx: ";
+        getline(cin, input);
+    
+    if (isValidPhoneNumber(input)) {
+        cout << "Valid." << endl;
+    
+    }
+    else {
+        cout << "Invalid." << endl;
+    
+    }
+    return 0;
+}
+
 
